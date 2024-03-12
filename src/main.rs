4a99cd8e496c8ffc636c1510ece7507f1f0431f6
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-03-12 17:44:36 +0530

    remove the need for a transpose function

--- src/main.rs
+++ src/main.rs
@@ -26,26 +26,37 @@ fn height_drop(board: &Vec<Vec<u8>>, row: i8, column: i8) -> u8{
     } else {0}
 }
 
-fn transpose(board: &Vec<Vec<u8>>) -> Vec<Vec<u8>>{
+fn rot90(board: &Vec<Vec<u8>>) -> Vec<Vec<u8>>{ // TODO
     let mut nboard = vec![vec![0; 6]; 7];
     for row in 0..board.len(){
         for col in 0..board[0].len(){
-            nboard[col][row] = board[row][col];
+            nboard[board[0].len() - col - 1][row] = board[row][col];
         }
     }
     nboard
 }
 
 
-fn pattern_counter(board: &Vec<Vec<u8>>, player: u8, patterns: &Vec<Pattern>) -> f64{
+fn pattern_counter(board: &Vec<Vec<u8>>, player: u8, patterns: &Vec<Pattern>, transpose: bool) -> f64{
     let mut result_map: HashMap<[u8; 4], u8> = patterns.iter().map(|e| (e.pattern, 0)).collect();
-    for rowi in 0..board.len(){
-        let mut map: HashMap<[u8; 4], usize> = patterns.iter().map(|e| (e.pattern, 0)).collect();
-        for coli in 0..board[0].len(){
-            apply_pattern_checking(&patterns, &mut map, board[rowi][coli], player, &mut result_map);
+    let map1: HashMap<[u8; 4], usize> = patterns.iter().map(|e| (e.pattern, 0)).collect();
+    let (r1, r2) = if transpose {
+        (0..board[0].len(), 0..board.len())
+    } else {
+        (0..board.len(), 0..board[0].len())
+    };
+    for rowi in r1{
+        let mut map: HashMap<[u8; 4], usize> = map1.clone();
+        for coli in r2.clone(){
+            let cell = if transpose {
+                board[coli][rowi]
+            } else {
+                board[rowi][coli]
+            };
+            apply_pattern_checking(&patterns, &mut map, cell, player, &mut result_map);
         }
     }
-    println!("{:?}", result_map);
+    // println!("{:?}", result_map);
     let mut resultant_value = 0.0;
     for p in patterns {
         resultant_value += p.weight * result_map.get(&p.pattern).unwrap_or(&0).to_owned() as f64;
@@ -104,7 +115,35 @@ fn pattern_counter_diagonal(board: &Vec<Vec<u8>>, player: u8, patterns: &Vec<Pat
         }
         coli = 0;
     }
-    println!("{:?}", result_map);
+    let mut rowi = 0;
+    for colistart in (0..board[0].len()).rev(){
+        let mut coli = colistart;
+        let mut map: HashMap<[u8; 4], usize> = patterns.iter().map(|e| (e.pattern, 0)).collect();
+        while rowi < board.len() {
+            apply_pattern_checking(&patterns, &mut map, board[rowi][coli], player, &mut result_map);
+            rowi += 1;
+            if coli == 0 {
+                break;
+            }
+            coli -= 1;
+        }
+        rowi = 0;
+    }
+    let mut coli = board[0].len() - 1;
+    for rowistart in 1..board.len(){
+        let mut rowi = rowistart;
+        let mut map: HashMap<[u8; 4], usize> = patterns.iter().map(|e| (e.pattern, 0)).collect();
+        while rowi < board.len() {
+            apply_pattern_checking(&patterns, &mut map, board[rowi][coli], player, &mut result_map);
+            rowi += 1;
+            if coli == 0 {
+                break;
+            }
+            coli -= 1;
+        }
+        coli = board[0].len() - 1;
+    }
+    // println!("{:?}", result_map);
     let mut resultant_value = 0.0;
     for p in patterns {
         resultant_value += p.weight * result_map.get(&p.pattern).unwrap_or(&0).to_owned() as f64;
@@ -129,13 +168,13 @@ fn evaluation_function(board: &Vec<Vec<u8>>, player: u8) -> f64{
 
         Pattern{pattern: [1, 1, 1, 1], weight: 10000.0}
     ];
-    let transposed = transpose(board);
-    let horiz = pattern_counter(board, player, &patterns);
-    let verti = pattern_counter(&transposed, player, &patterns);
+    // let transposed = rot90(board);
+    let horiz = pattern_counter(board, player, &patterns, false);
+    let verti = pattern_counter(board, player, &patterns, true);
     let diag1 = pattern_counter_diagonal(&board, player, &patterns);
-    let diag2 = pattern_counter_diagonal(&transposed, player, &patterns);
+    // let diag2 = pattern_counter_diagonal(&transposed, player, &patterns);
     
-    horiz + verti + diag1 + diag2
+    horiz + verti + diag1// + diag2
 }
 
 
@@ -148,8 +187,14 @@ fn main() {
         vec![2, 0, 2, 1, 1, 1, 1],
         vec![1, 2, 2, 1, 1, 1, 2]
     ];
-    println!("{}", evaluation_function(&board, 1));
-    println!("{}", evaluation_function(&board, 2));
+    for i in 0..7*7*7*7*7{
+        evaluation_function(&board, 1);
+        evaluation_function(&board, 2);
+    }
+    // println!("{}", evaluation_function(&board, 1));
+    // println!("{}", evaluation_function(&board, 2));
+    // println!("{}", pattern_counter_diagonal(&board, 1, &vec![Pattern{pattern: [1, 1, 0, 1], weight: 1.0}]));
+    print!("DONE!")
     // println!("{:?}", pattern_counter(&board, 1, &vec![
     //         Pattern{pattern: [1, 1, 0, 1], weight: 1.0},
     //         Pattern{pattern: [1, 0, 0, 1], weight: 1.0},
